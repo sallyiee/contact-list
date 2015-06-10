59d7ed6c8519824e5102310114ab9f97af4ee4b3
sallyiee <amy.elly.lee@gmail.com>
2015-06-10 17:51:26 +1000

    Updated heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -438,7 +438,7 @@ module.exports = function (grunt) {
     },
     heroku: {
       options: {
-        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        remote: 'git@heroku.com:salty-inlet-6186.git',
         branch: 'master',
         tag: pkg.version
       }
