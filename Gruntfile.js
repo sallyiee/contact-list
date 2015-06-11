c75718d5cd03a7bdfa61af26f6ced516b6c00795
sallyiee <amy.elly.lee@gmail.com>
2015-06-11 15:35:56 +1000

    Changing heroku address

--- Gruntfile.js
+++ Gruntfile.js
@@ -438,7 +438,7 @@ module.exports = function (grunt) {
     },
     heroku: {
       options: {
-        remote: 'git@heroku.com:rocky-plains-7241.git',
+        remote: 'git@heroku.com:mysterious-oasis-1515.git',
         branch: 'master',
         tag: pkg.version
       }
